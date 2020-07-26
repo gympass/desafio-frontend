6040389699aa9559060a33a505fa9b752c9b5e49
Rodrigo Lourenço <rsilvape@gmail.com>
2020-07-26 14:09:59 -0300

    Adicionando styled-components e reset css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ResetCSS from './styles/ResetCSS.styles';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ResetCSS />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
